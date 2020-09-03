741dff97d43252609aa3eab49584980cfebcb5b0
Anthony Rojas <anthony.rojas095@gmail.com>
2020-09-03 09:39:16 -0700

    Added get users route for SuperAdmin users

--- routes/User.js
+++ routes/User.js
@@ -2,6 +2,7 @@ const Auth = require('../middleware/auth');
 const UserController = require('../controllers/UserController');
 module.exports = (router) => {
     router.get('/', Auth.validateToken, UserController.getUser);
+    router.get('/all', Auth.validateToken, UserController.getUsers);
     router.get('/:id', Auth.validateToken, UserController.getUser);
     router.post('/', UserController.addAccount);
     router.post('/admin', Auth.validateToken, UserController.addAccount);
