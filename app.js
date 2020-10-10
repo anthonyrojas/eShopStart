1dc9ca2acc7455f47935fad774ddea9cc7134823
Anthony Rojas <anthony.rojas095@gmail.com>
2020-10-10 02:39:19 -0700

    Added authorization header to the list of allowed headers for CORS

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use((req, res, next)=>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
